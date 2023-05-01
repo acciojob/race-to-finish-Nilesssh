2f5b5e7f66937fea4070ff2a48317ab83ed5ae6e
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-01 13:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ window.promises = [];
 // Do not change the code above this
 // add your promises to the array `promises'
 
-const promises = [
-  new Promise((resolve) => setTimeout(() => resolve(1), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(2), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(3), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(4), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(5), Math.random() * 5000 + 1000)),
-];
+
 
 Promise.any(promises)
   .then((result) => {
